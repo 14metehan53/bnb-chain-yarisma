db873cae4fca1b437c0dbbbbbb3f58c2a99f7013
Metehan <61335521+14metehan53@users.noreply.github.com>
2023-10-30 20:38:08 +0300

    Add files via upload

---
+++ Gold.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract RISEIN is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("RISEIN", "RSN") {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burnFrom(address account, uint256 amount) public override onlyOwner {
+        _burn(account, amount);
+    }
+}
